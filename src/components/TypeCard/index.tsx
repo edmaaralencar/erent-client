305dc9065728dee5bb049590f6b425c4a67eb3c2
Edmar Rocha Alencar <edmaaralencar1@gmail.com>
2023-05-02 13:22:07 -0300

    refactor: change button to link in TypeCard component

--- src/components/TypeCard/index.tsx
+++ src/components/TypeCard/index.tsx
@@ -2,6 +2,7 @@ import { ITEM_ANIMATION } from '../../utils/animations'
 import Image from 'next/image'
 import Button from '../Button'
 import * as S from './styles'
+import Link from 'next/link'
 
 type TypeCardProps = {
   name: string
@@ -16,7 +17,11 @@ function TypeCard({ name, description, href, type }: TypeCardProps) {
       <S.Content>
         <strong>{name}</strong>
         <p>{description}</p>
-        <Button size="small">Veja mais</Button>
+        <Link passHref legacyBehavior href="/properties">
+          <Button size="small" as="a">
+            Veja mais
+          </Button>
+        </Link>
       </S.Content>
       <S.ImageWrapper>
         <Image src="/image.png" alt="image" fill />
